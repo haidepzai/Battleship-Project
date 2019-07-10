f84d88468bc41e144e7c20aefb933e1c42964bef
Dubiel Sebastian <sd114@r135-127.pool.mintern.mi.hdm-stuttgart.de>
2019-07-10 13:11:51 +0200

    Added log to Exception

--- src/main/java/de/hdm_stuttgart/mi/sd2/Exceptions/IllegalFactoryArgument.java
+++ src/main/java/de/hdm_stuttgart/mi/sd2/Exceptions/IllegalFactoryArgument.java
@@ -1,8 +1,14 @@
 package de.hdm_stuttgart.mi.sd2.Exceptions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class IllegalFactoryArgument extends Exception{
+
+    private static Logger log = LogManager.getLogger(IllegalFactoryArgument.class);
 
     public IllegalFactoryArgument( String message ) {
         super(message);
+        log.error("A fatal error has occurred!");
     }
 }
