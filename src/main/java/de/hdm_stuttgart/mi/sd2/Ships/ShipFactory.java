e533a357941ba101147d67edcc9c7a8752ec0f57
lh108 <1lM$d=Hw!!>
2019-07-18 15:09:31 +0200

    Log-level changed, javadoc comment added

--- src/main/java/de/hdm_stuttgart/mi/sd2/Ships/ShipFactory.java
+++ src/main/java/de/hdm_stuttgart/mi/sd2/Ships/ShipFactory.java
@@ -4,6 +4,12 @@ import de.hdm_stuttgart.mi.sd2.Exceptions.IllegalFactoryArgument;
 import de.hdm_stuttgart.mi.sd2.Interfaces.IShip;
 
 public class ShipFactory {
+    /**
+     * Creation of ship types
+     * @param i Ship type enum
+     * @return Object of ship type
+     * @throws IllegalFactoryArgument Ship type not found
+     */
     public static synchronized IShip createShip(IShip.ShipType i) throws IllegalFactoryArgument {
         switch (i) {
             case DESTROYER: return new Destroyer() ;
